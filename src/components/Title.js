20602e2edb5ff84ca174cd9561fd9dbb1c327f9f
sticklemate <sticklemate@gmail.com>
2019-12-21 15:04:27 +1100

    Initial commit

---
+++ src/components/Title.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+class Title extends React.Component {
+
+
+render(){
+
+return(
+<div>
+
+<h1>Weather Finder app</h1>
+<p>Find out temperateure conditions and more...</p>
+</div>
+);
+
+}
+
+
+}
+
+
+export default Title;

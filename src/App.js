3ad50d1b40a66bef307fead1a7bfcc0091e97df8
sticklemate <sticklemate@gmail.com>
2019-12-21 15:48:54 +1100

    Add API calls to openweather.com

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 /*jshint esversion: 6 */
 import React from "react";
 import Title from "./components/Title";
+import Form from "./components/Form";
+import Weather from "./components/Weather";
+
+const API_KEY="7e070ed26fa13b00fb40a9901670b645";
 
 class App extends React.Component {
+
+getWeather = async () => {
+
+const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=Melbourne,AUS&appid=7e070ed26fa13b00fb40a9901670b645`);
+
+const data = await api_call.json();
+console.log(data);
+
+}
 
 render(){
 
@@ -11,6 +24,8 @@ render(){
     <div>
 
       <Title />
+      <Form getWeather={this.getWeather} />
+      <Weather />
     </div>
   
     );

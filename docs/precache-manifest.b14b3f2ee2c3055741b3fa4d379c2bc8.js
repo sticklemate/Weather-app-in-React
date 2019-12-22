917df98b384221ed99823ce0428287fcd1a31fa1
sticklemate <sticklemate@gmail.com>
2019-12-22 20:50:06 +1100

    Build ready

---
+++ docs/precache-manifest.b14b3f2ee2c3055741b3fa4d379c2bc8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a478eaef15eddd5998aa033bc9d2b9b0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0fa37a4491f3c97b251e",
+    "url": "/static/css/main.051dc80a.chunk.css"
+  },
+  {
+    "revision": "4923ce9c73156342e2eb",
+    "url": "/static/js/2.84d0ca4c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.84d0ca4c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0fa37a4491f3c97b251e",
+    "url": "/static/js/main.c3c558ae.chunk.js"
+  },
+  {
+    "revision": "955fc2ae179047931c61",
+    "url": "/static/js/runtime-main.0e90a2f9.js"
+  }
+]);

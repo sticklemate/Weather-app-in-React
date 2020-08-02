17ad89158b66987434e9c0821c46ab48e30cd7d7
sticklemate <sticklemate@gmail.com>
2020-08-02 11:34:16 +0000

    Integrate Travis CI

---
+++ test/index-spec.js
@@ -0,0 +1,10 @@
+it('Renders TicTacToe game board', () => {
+    // Write your test case here
+    console.log("Sample test");
+  });
+
+
+it('Renders without crashing', () => {
+    // Write your test case here
+    console.log("Sample test");
+  });
